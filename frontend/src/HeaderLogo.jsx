959b570036ecd08a8ebf725f28ea73053a7c0a42
Jess-An-Associates <jessica.chan@an-associates.com>
2023-02-23 12:51:44 -0500

    Landing Page UI

--- frontend/src/HeaderLogo.jsx
+++ frontend/src/HeaderLogo.jsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { logo } from "./assets";
+import { logo, sidebckgnd } from "./assets";
 import "../src/App.css";
 import { Link } from "react-router-dom";
+import "./HeaderLogo.css";
+
+
 
 function HeaderLogo() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <img
-        src={logo}
-        style={{ width: "15%", height: "30%", marginTop: "2%" }}
-      />
-      <h2>AI ProdCopywriter</h2>
-    </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <img
+          src={logo}
+          style={{ width: "15%", height: "30%", marginTop: "2%" }}
+        />
+        <h2>AI ProdCopywriter</h2>
+      </div>
+    
   );
 }
 
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,1 +1,7 @@
 export { default as logo } from "../assets/Logo.png";
+export { default as sidebckgnd } from "../assets/Sidebar.png";
+export { default as robot } from "../assets/Product AI.png";
+export { default as fulllogo } from "../assets/FullLogo.png";
+
+
+
