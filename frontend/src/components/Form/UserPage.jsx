97bab901e5f02977de379f7350485e2da1824778
mohitlekh <workmohitlekhrjani>
2023-03-07 03:26:58 +0530

    mobile view

--- frontend/src/components/Form/UserPage.jsx
+++ frontend/src/components/Form/UserPage.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./form.css";
 import { Grid, Button } from "@mui/material";
 import Loader from "../loader/loader";
 import HeaderLogo from "../../HeaderLogo";
 import SidebarMenu from "../../SidebarMenu";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Footer from "../../Footer";
+import SidebarMobile from "../mobile-view/SidebarMobile";
+import Carousel from "react-bootstrap/Carousel";
 
 function Dummy(props) {
   const [tone, setTone] = useState("Friendly");
@@ -14,6 +18,8 @@ function Dummy(props) {
   const [loading, setLoading] = useState(false);
   const [Copied, setCopied] = useState();
   const [AllCopied, setAllCopied] = useState();
+  const [showMenu, setShowMenu] = useState(false);
+  console.log(showMenu);
 
   const textAreaRef = useRef(null);
 
@@ -75,25 +81,35 @@ function Dummy(props) {
   const handleTone = (e) => {
     setTone(e);
   };
-
+  let arr = [];
   return (
     <div className="category-page">
       <div className="category-title">
-        <h2>
+        <h2 className="category-title-text">
           <strong>{props.category}</strong>
         </h2>
+        <div className="header-mobile-view">
+          <HeaderLogo />
+          <div className="hamburger-menu">
+            <h2 onClick={() => setShowMenu(!showMenu)}>
+              <GiHamburgerMenu />
+            </h2>
+          </div>
+        </div>
       </div>
       <div className="main-division">
         <Grid container spacing={4}>
-        <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
-          <div className="logo-panel">
-            <HeaderLogo />
-            <SidebarMenu />
-          </div>
-        </Grid>
-          <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
-            <form className="form">
-               
+          {/* siderbar */}
+          <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
+            <div className="logo-panel">
+              <HeaderLogo />
+              <SidebarMenu />
+            </div>
+          </Grid>
+          {/* Main Form */}
+          <div className="main-form-desktop-view">
+            <Grid item xs={5} sm={5} md={5} lg={5} xl={5}>
+              <form className="form">
                 <div className="input_one">
                   <h5>
                     <strong>{props.inputOneTitle}</strong>
@@ -101,7 +117,7 @@ function Dummy(props) {
                   <textarea
                     value={inputOne}
                     onChange={(e) => setInputOne(e.target.value)}
-                  placeholder={props.placeholderOne}
+                    placeholder={props.placeholderOne}
                   />
                   {/* {console.log(inputOne, inputTwo, tone)} */}
                 </div>
@@ -114,7 +130,7 @@ function Dummy(props) {
                     <textarea
                       value={inputTwo}
                       onChange={(e) => setInputTwo(e.target.value)}
-                    placeholder={props.placeholderTwo}
+                      placeholder={props.placeholderTwo}
                     />
                   </div>
                 ) : (
@@ -128,8 +144,14 @@ function Dummy(props) {
                     </h5>
                     <Grid container justifyContent="center" spacing={4}>
                       <Grid item xs={3} sm={3} md={3}>
-                        <Button className={tone == "Friendly"? "tone-btn-selected": "tone-btn"} onClick={(e) => handleTone("Friendly")}>
-                     
+                        <Button
+                          className={
+                            tone == "Friendly"
+                              ? "tone-btn-selected"
+                              : "tone-btn"
+                          }
+                          onClick={(e) => handleTone("Friendly")}
+                        >
                           Friendly
                         </Button>
                       </Grid>
@@ -182,10 +204,10 @@ function Dummy(props) {
                     Submit
                   </button>
                 </div>
-              
-            </form>
-          </Grid>
-
+              </form>
+            </Grid>
+          </div>
+          {/* results */}
           <Grid item xs={4} sm={12} md={4}>
             <div className="category-title mr-2">
               <h2>
@@ -259,6 +281,192 @@ function Dummy(props) {
             )}
           </Grid>
         </Grid>
+      </div>
+      <div className="mobile-view-product">
+        <ul aria-expanded={showMenu}>
+          <li>
+            <SidebarMobile />
+          </li>
+        </ul>
+        <div className="main-form-mobile">
+          <p>{props.category}</p>
+          <Grid item xs={12}>
+            <form className="form">
+              <div className="input_one">
+                <h5>
+                  <strong>{props.inputOneTitle}</strong>
+                </h5>
+                <textarea
+                  value={inputOne}
+                  onChange={(e) => setInputOne(e.target.value)}
+                  placeholder={props.placeholderOne}
+                />
+                {/* {console.log(inputOne, inputTwo, tone)} */}
+              </div>
+              {props.inputTwo ? (
+                <div className="input_one">
+                  <h5>
+                    <strong>{props.inputTwoTitle}</strong>
+                  </h5>
+
+                  <textarea
+                    value={inputTwo}
+                    onChange={(e) => setInputTwo(e.target.value)}
+                    placeholder={props.placeholderTwo}
+                  />
+                </div>
+              ) : (
+                <></>
+              )}
+
+              {props.toneInput ? (
+                <div className="input_three">
+                  <h5>
+                    <strong>Select a tone</strong>
+                  </h5>
+                  <Grid container justifyContent="center" spacing={4}>
+                    <Grid item xs={6}>
+                      <Button
+                        className={
+                          tone == "Friendly" ? "tone-btn-selected" : "tone-btn"
+                        }
+                        onClick={(e) => handleTone("Friendly")}
+                      >
+                        Friendly
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Button
+                        className={
+                          tone == "Professional"
+                            ? "tone-btn-selected"
+                            : "tone-btn"
+                        }
+                        onClick={(e) => handleTone("Professional")}
+                      >
+                        Professional
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Button
+                        className={
+                          tone == "Empathetic"
+                            ? "tone-btn-selected"
+                            : "tone-btn"
+                        }
+                        onClick={(e) => handleTone("Empathetic")}
+                      >
+                        Empathetic
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Button
+                        className={
+                          tone == "Bold" ? "tone-btn-selected" : "tone-btn"
+                        }
+                        onClick={(e) => handleTone("Bold")}
+                      >
+                        Bold
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              ) : (
+                <></>
+              )}
+
+              <div className="submitButton">
+                <button
+                  onClick={() => handleSubmit(path)}
+                  disabled={loading}
+                  variant="contained"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </Grid>
+        </div>
+        <div className="result-mobile-view">
+          <Grid item xs={12}>
+            <div className="category-title mr-2">
+              <h2>
+                <strong>Result</strong>
+              </h2>
+            </div>
+            {/* {console.log("data",data)} */}
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                {data != undefined ? (
+                  <>
+                    <button
+                      className="cpyall-btn"
+                      onClick={() => copyToAllClipboard(data)}
+                    >
+                      {" "}
+                      Copy All
+                    </button>
+                    {AllCopied ? (
+                      <h1 className="text-cp-al">ALL RESULTS COPIED ! </h1>
+                    ) : (
+                      ""
+                    )}
+
+                    <div className="output-container-mobile-view">
+                      {data.map((item, i) => {
+                        return (
+                          <div className="output-layout-mobile-view">
+                            <Grid container>
+                              <Carousel>
+                                <Carousel.Item>
+                                  <Grid item xs={12}>
+                                    <div className="ot-hd-mobile-view">
+                                      <div>Result {i + 1}</div>
+                                    </div>
+                                  </Grid>
+                                  <Grid item xs={12}>
+                                    <div className="hd-cp-mobile-view">
+                                      <button
+                                        className="cpy-btn"
+                                        onClick={() => copyToClipboard(item, i)}
+                                      >
+                                        Copy
+                                      </button>
+                                      {Copied == i + 1 ? (
+                                        <h1 className="text-status-mobile-view">
+                                          Result {i + 1} Copied !{" "}
+                                        </h1>
+                                      ) : (
+                                        ""
+                                      )}
+                                    </div>
+                                  </Grid>
+                                </Carousel.Item>
+                              </Carousel>
+                              ;
+                            </Grid>
+                            <div
+                              className="ot-bd-mobile-view"
+                              ref={textAreaRef}
+                            >
+                              <h6 className="instruction-desc">
+                                {item.text.trim()}
+                              </h6>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </>
+                ) : (
+                  <>{/* <div className="err-msg">Oops Try Again !</div> */}</>
+                )}
+              </>
+            )}
+          </Grid>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/mobile-view/SidebarMobile.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import catagoryList from "../../components/json/category-list.json";
+import Dropdown from "react-bootstrap/Dropdown";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import "../mobile-view/sidebarmobile.css";
+import { Grid } from "@mui/material";
+import { footerlogo } from "../../assets";
+
+function SidebarMobile() {
+  return (
+    <>
+      <div className="sidebar-mobile-wrapper">
+        <div className="sidebar-mobile">
+          <Link to="/" className="sidebar-mobile-home-btn">
+            {" "}
+            Home
+          </Link>
+          {catagoryList.map((cat, i) => {
+            return (
+              <Accordion>
+                <Card>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header style={{ fontSize: "20px !important" }}>
+                      {cat.parentcategory}
+                    </Accordion.Header>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        <hr className="menu-separator" />
+                        {cat.childcategory.map((menu) => {
+                          return (
+                            <Dropdown.Item
+                              id={"bs-item-override"}
+                              style={{ fontSize: "20px !important" }}
+                            >
+                              <Link
+                                to={menu.url}
+                                className="category-link"
+                                style={{ fontSize: "20px !important" }}
+                              >
+                                {menu.name}
+                              </Link>
+                            </Dropdown.Item>
+                          );
+                        })}
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Accordion.Item>
+                </Card>
+              </Accordion>
+            );
+          })}
+        </div>
+      </div>
+      <div className="footer">
+        <Grid container spacing={0}>
+          <Grid item xs={8} sm={2} md={2} lg={2} xl={2}>
+            <img src={footerlogo} alt="AI ProdCopywriter logo" />
+          </Grid>
+          <Grid item xs={4} sm={6} md={8} lg={2} xl={2}>
+            {`© 2023 AI ProdCopywriter`}
+          </Grid>
+
+          <Grid item xs={8} sm={2} md={2} lg={2} xl={2}>
+            {`support@smartecomtech.com`}
+          </Grid>
+
+          <Grid item xs={4} sm={2} md={2} lg={2} xl={2}>
+            {`Smart Ecom Tech`}
+          </Grid>
+
+          <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+            {/* {`(*Contact Detail*)`} */}
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+}
+
+export default SidebarMobile;
