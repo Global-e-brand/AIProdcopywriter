64e8e5d59b73a56c778d88718247bda20ccbde6e
AkashM-eng <akash@an-associates.com>
2023-03-14 22:31:15 +0530

    getGoogle id and meta id

--- backend/general/common.function.js
+++ backend/general/common.function.js
@@ -2,3 +2,11 @@ export function getUserId(req) {
   // console.log("request",req);
   return req?.session?.passport?.user?._id || req?.session?.passport?.user?.id;
 }
+
+export function getGoogleId(){
+
+}
+
+export function getMetaId(){
+  
+}
