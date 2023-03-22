2a797ecc42b1e0d5d8173cc85ff121aa4c491f35
AkashM-eng <akash@an-associates.com>
2023-03-22 20:15:40 +0530

    Dashboard Components

---
+++ frontend/src/components/Dashboard/Analytics.jsx
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react';
+import { ActiveUsers } from './ActiveUsers';
+
+export function Analytics(props) {
+  return (
+    <div>
+        <ActiveUsers />
+    </div>
+  );
+};
