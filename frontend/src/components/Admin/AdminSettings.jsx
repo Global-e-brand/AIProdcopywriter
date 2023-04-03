9560fe61d79ffc421fbc304d0e31441114af1029
Ali <ali@an-associates.com>
2023-04-04 03:51:29 +0530

    add admin page responsiveness

--- frontend/src/components/Admin/AdminSettings.jsx
+++ frontend/src/components/Admin/AdminSettings.jsx
@@ -1,108 +1,116 @@
 import * as React from "react";
 import "./adminsettings.css";
+import SidebarMenu from "../../SidebarMenu";
+import { Grid } from "@mui/material";
 export default function AdminSettings() {
   return (
-    <div className="">
+    <div>
       <div className="adminBar-desktopView">
         <p>Admin Settings</p>
       </div>
-      <div className="adminSettingsContainer-desktopView">
-        <div className="modifyPricesContainer-desktopView">
-          <div className="modifyPricesHeader-desktopView">
-            Modify All Prices
-          </div>
-          <div className="modifyPrices-desktopView ">
-            <div className="Plan-desktopView">
-              <div className="planHeader-desktopView">
-                <p className="planLabel-desktopView">Basic Plan</p>
-                <p className="currentPriceLabel-desktopView">
-                  Currently $99 per month
-                </p>
+      <Grid container>
+        <Grid item xs={3}>
+          <SidebarMenu />
+        </Grid>
+        <Grid item xs={9}>
+          <div className="parent-container-desktopView">
+            <div className="adminSettingsContainer-desktopView">
+              <div className="modifyPricesContainer-desktopView">
+                <div className="modifyPricesHeader-desktopView">
+                  Modify All Prices
+                </div>
+                <div className="modifyPrices-desktopView ">
+                  <div className="Plan-desktopView">
+                    <div className="planHeader-desktopView">
+                      <p className="planLabel-desktopView">Basic Plan</p>
+                      <p className="currentPriceLabel-desktopView">
+                        Currently $99 per month
+                      </p>
+                    </div>
+                    <div className="modifyPriceInputContainer-desktopView">
+                      <input
+                        placeholder="$29.99"
+                        name="modifyPriceInput-desktopView"
+                        className="modifyPriceInput-desktopView"
+                      ></input>
+                      <button>Change</button>
+                    </div>
+                  </div>
+                  <div className="Plan-desktopView">
+                    <div className="planHeader-desktopView">
+                      <p className="planLabel-desktopView">Standard Plan</p>
+                      <p className="currentPriceLabel-desktopView">
+                        Currently $99 per month
+                      </p>
+                    </div>
+                    <div className="modifyPriceInputContainer-desktopView">
+                      <input
+                        placeholder="Input Value..."
+                        name="modifyPriceInput-desktopView"
+                        className="modifyPriceInput-desktopView"
+                      ></input>
+                      <button>Change</button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="modifyPriceInputContainer-desktopView">
-                <input
-                  placeholder="$29.99"
-                  name="modifyPriceInput-desktopView"
-                  className="modifyPriceInput-desktopView"
-                ></input>
-                <button>Change</button>
+              <div className="customiseUserPricesContainer-desktopView">
+                <div className="customiseUserPricesHeader-desktopView">
+                  Customise user Prices
+                </div>
+                <div className="customiseUserPrices-desktopView">
+                  <p id="mostRecurringUsersHeader-desktopView">
+                    Most Recurring users
+                  </p>
+                  <div className="mostRecurringUsers-desktopView">
+                    <label className="mostRecurringUser-desktopView">
+                      <input type="checkbox" value="1" />
+                      <span>Random Username 1</span>
+                    </label>
+                    <label className="mostRecurringUser-desktopView">
+                      <input type="checkbox" value="1" />
+                      <span>Random Username 2</span>
+                    </label>
+                    <label className="mostRecurringUser-desktopView">
+                      <input type="checkbox" value="1" />
+                      <span>Random Username 3</span>
+                    </label>
+                    <label className="mostRecurringUser-desktopView">
+                      <input type="checkbox" value="1" />
+                      <span>Random Username 4</span>
+                    </label>
+                    <label className="mostRecurringUser-desktopView">
+                      <input type="checkbox" value="1" />
+                      <span>Random Username 5</span>
+                    </label>
+                  </div>
+                  <div className="customiseUserPrices-input-container">
+                    <div className="customiseUserPricesInput-desktopView inputUser-desktopView">
+                      <input
+                        placeholder="Input a user..."
+                        name="modifyPriceInput-desktopView"
+                        className="modifyPriceInput-desktopView"
+                      ></input>
+                      <button>Change</button>
+                    </div>
+                    <div className="customiseUserPricesInput-desktopView ">
+                      <span className="input-span">
+                        <input
+                          placeholder="Input Value..."
+                          name="modifyPriceInput-desktopView"
+                          className="modifyPriceInput-desktopView"
+                          // onClick={handleClick}
+                        ></input>
+                      </span>
+                      <button>Change</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="Plan-desktopView">
-              <div className="planHeader-desktopView">
-                <p className="planLabel-desktopView">Standard Plan</p>
-                <p className="currentPriceLabel-desktopView">
-                  Currently $99 per month
-                </p>
-              </div>
-              <div className="modifyPriceInputContainer-desktopView">
-                <input
-                  placeholder="Input Value..."
-                  name="modifyPriceInput-desktopView"
-                  className="modifyPriceInput-desktopView"
-                ></input>
-                <button>Change</button>
-              </div>
-            </div>
           </div>
-        </div>
-        <div className="customiseUserPricesContainer-desktopView">
-          <div className="customiseUserPricesHeader-desktopView">
-            Customise user Prices
-          </div>
-          <div className="customiseUserPrices-desktopView">
-            <p id="mostRecurringUsersHeader-desktopView">
-              Most Recurring users
-            </p>
-            <div className="mostRecurringUsers-desktopView">
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 1</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 2</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 3</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 4</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 5</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 6</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 7</p>
-              </div>
-              <div className="mostRecurringUser-desktopView">
-                <p>Random Username 8</p>
-              </div>
-            </div>
-            <div className="customiseUserPrices-input-container">
-              <div className="customiseUserPricesInputContainer-desktopView inputUser-desktopView">
-                <input
-                  placeholder="Input a user..."
-                  name="modifyPriceInput-desktopView"
-                  className="modifyPriceInput-desktopView"
-                ></input>
-                <button>Change</button>
-              </div>
-              <div className="customiseUserPricesInputContainer-desktopView ">
-                <span className="input-span">
-                  <input
-                    placeholder="Input Value..."
-                    name="modifyPriceInput-desktopView"
-                    className="modifyPriceInput-desktopView"
-                  ></input>
-                </span>
-                <button>Change</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
